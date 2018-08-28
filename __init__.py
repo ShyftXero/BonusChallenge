b0e2444e996b9e352d529416740cb48cac154a2b
= <=>
2018-08-28 12:30:52 -0500

    reimplemented as an independent plugin

--- __init__.py
+++ __init__.py
@@ -1,21 +1,30 @@
+from flask import render_template, render_template_string, request, redirect, jsonify, url_for, session, Blueprint, abort, make_response
+
+from werkzeug.routing import Rule
+
+import json
+from CTFd.challenges import *
 from CTFd.plugins.challenges import BaseChallenge, CHALLENGE_CLASSES
 from CTFd.plugins import register_plugin_assets_directory
 from CTFd.plugins.keys import get_key_class
 from CTFd.models import db, Solves, WrongKeys, Keys, Challenges, Files, Tags, Teams, Awards
 from CTFd import utils
 import math
+import os
 
 
 class BonusChallenge(BaseChallenge):
     id = "bonus"  # Unique identifier used to register challenges
     name = "bonus"  # Name of a challenge type
     templates = {  # Handlebars templates used for each aspect of challenge editing & viewing
-        'create': '/plugins/BonusChallenge/assets/bonus-challenge-create.hbs',
-        'update': '/plugins/BonusChallenge/assets/bonus-challenge-update.hbs',
+        'create': '/plugins/BonusChallenge/assets/bonus-challenge-create.njk',
+        'update': '/plugins/BonusChallenge/assets/bonus-challenge-update.njk',
+        'modal': '/plugins/BonusChallenge/assets/bonus-challenge-modal.njk'
     }
     scripts = {  # Scripts that are loaded when a template is loaded
         'create': '/plugins/BonusChallenge/assets/bonus-challenge-create.js',
         'update': '/plugins/BonusChallenge/assets/bonus-challenge-update.js',
+        'modal': '/plugins/BonusChallenge/assets/bonus-challenge-modal.js'
     }
 
     @staticmethod
@@ -31,7 +40,7 @@ class BonusChallenge(BaseChallenge):
         # Create challenge
         chal = BonusChallenges(
             name=request.form['name'],
-            description=request.form['desc'],
+            description=request.form['description'],
             value=request.form['value'],
             category='Bonus Flag',
             type=request.form['chaltype'],
@@ -126,10 +135,11 @@ class BonusChallenge(BaseChallenge):
         :param request: The request the user submitted
         :return: (boolean, string)
         """
+
         provided_key = request.form['key'].strip()
         chal_keys = Keys.query.filter_by(chal=chal.id).all()
         for chal_key in chal_keys:
-            if get_key_class(chal_key.key_type).compare(chal_key.flag, provided_key):
+            if get_key_class(chal_key.type).compare(chal_key, provided_key):
                 return True, 'Correct'
         return False, 'Incorrect'
 
@@ -153,7 +163,7 @@ class BonusChallenge(BaseChallenge):
         db.session.close()
 
     @staticmethod
-    def fail(team, chal, request):
+    def fail(team, chal=None, request=None):
         """
         This method is used to insert WrongKeys into the database in order to mark an answer incorrect.
 
@@ -163,7 +173,9 @@ class BonusChallenge(BaseChallenge):
         :return:
         """
         provided_key = request.form['key'].strip()
-        wrong = WrongKeys(teamid=team.id, chalid=chal.id, ip=utils.get_ip(request), flag=provided_key)
+        #there is no valid chal.id for a key that doesn't exist... so use a placeholder? hopefully it won't puke...
+        wrong = WrongKeys(teamid=team.id, chalid=31337, ip=utils.get_ip(request), flag=provided_key)
+        #this isn't being utilized... watch console for bad flags....
         db.session.add(wrong)
         db.session.commit()
         db.session.close()
@@ -184,4 +196,64 @@ class BonusChallenges(Challenges):
 def load(app):
     app.db.create_all()
     CHALLENGE_CLASSES['bonus'] = BonusChallenge
+
+    #https://github.com/CTFd/CTFd/wiki/Plugins#replacing-templates
+    # dir_path = os.path.dirname(os.path.realpath(__file__))
+    # template_path = os.path.join(dir_path, 'bonus.html')
+    # utils.override_template('bonus.html', open(template_path).read())
+
+    @app.route('/bonus', methods=['POST', 'GET'])
+    @authed_only #require logged in team to view.
+    @during_ctf_time_only
+    def bonus():
+        if utils.ctf_paused():
+            return redirect('/scoreboard')
+        #try to open the file itself because I can't figure out how to change template path... this way it lives in the assets path
+        location = (app.instance_path + '/plugins/BonusChallenge/assets/bonus.html').replace("instance", "CTFd")
+        print(location)
+        file = open(location, 'r')
+        template_string = file.read()
+        file.close()
+        # print(template_string)
+
+        #bonus flags already captured by this team
+        already_solved = Solves.query.join(Challenges).filter(Solves.teamid == session.get('id'), Challenges.type == 'bonus').all()
+
+        if request.method == 'GET':
+            return render_template_string(template_string, already_solved=already_solved, message='' )
+            # return render_template('bonus.html', already_solved=already_solved, message='')
+
+        #if you get here, you're submitting a flag / POSTING
+
+
+        unsolved_bonuses = Challenges.query.filter(Challenges.type == 'bonus').all()
+
+        team = Teams.query.filter(Teams.id == session.get('id')).first()
+        provided_valid_key = False
+        for chal in unsolved_bonuses: #searching for a lock that this key will open
+            status, message = BonusChallenge.attempt(chal, request) #status will be True if valid key presented
+            if status == True: # presented valid flag
+                resubmitted_key = False
+                for solved_chal in already_solved: #searching for a resubmitted key
+                    if chal.id == solved_chal.id:
+                        message = "You've already submitted this flag."
+                        resubmitted_key = True
+                        break
+
+                if resubmitted_key:
+                    break
+
+                #if you got here,you submitted a flag which has not already been submitted by this team and get points
+                provided_valid_key = True
+                BonusChallenge.solve(team=team, chal=chal, request=request) #points given as an Award
+                break
+
+        if provided_valid_key == False or resubmitted_key == True:
+            # BonusChallenge.fail(team=team, chal=chal, request=request)
+            print(f"team '{team.name}' submitted a bonus flag: '{request.form.get('key').strip()}'")
+
+
+        return render_template_string(template_string, already_solved=already_solved, message=message )
+        # return render_template('bonus.html', already_solved=already_solved, message=message)
+
     register_plugin_assets_directory(app, base_path='/plugins/BonusChallenge/assets/')
